71511b869695f7dac7e12fe6400a4d6dfb2d1652
Ed Langley <elangley@cj.com>
2018-11-03 23:19:15 -0700

    Another commit, another CI issue down

--- clone-all-the-things.sh
+++ clone-all-the-things.sh
@@ -3,8 +3,8 @@ cd $HOME
 
 wget -q-O- https://srv2.elangley.org/~edwlan/sbcl-static.tgz|tar vzx 
 
-git clone --depth=1 https://gitlab.com/fiddlerwoaroof/osicat.git ~/sbcl/quicklisp/local-projects/osicat.git
-git clone --depth=1 https://gitlab.com/fiddlerwoaroof/cffi.git ~/sbcl/quicklisp/local-projects/cffi.git
+git clone --depth=1 https://gitlab.com/fiddlerwoaroof/osicat.git ~/sbcl/quicklisp/local-projects/osicat
+git clone --depth=1 https://gitlab.com/fiddlerwoaroof/cffi.git ~/sbcl/quicklisp/local-projects/cffi
 
 git clone --depth=1 https://github.com/fiddlerwoaroof/daydreamer.git ~/sbcl/quicklisp/local-projects/daydreamer/
 git clone --depth=1 https://github.com/fiddlerwoaroof/fwoar.lisputils.git ~/sbcl/quicklisp/local-projects/fwoar.lisputils/
--- make.sh
+++ make.sh
@@ -3,4 +3,3 @@ ls -R ~/sbcl
 ~/sbcl/bin/sbcl --no-userinit --disable-debugger --load ~/sbcl/quicklisp/setup.lisp --load build.lisp
 ls ~/sbcl/quicklisp/local-projects/daydreamer/
 ./daydreamer --self-test
-rm -rf ~/sbcl/quicklisp/local-projects/*
