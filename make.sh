6904ae48925ce2e34ac6b4736ee22818ff0ea584
Ed Langley <elangley@cj.com>
2018-11-03 23:24:17 -0700

    add env vars

--- make.sh
+++ make.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+export ASDF_OUTPUT_TRANSLATIONS="/:" SBCL_HOME="$HOME/sbcl/lib/sbcl/" CC="clang"
+
 ls -R ~/sbcl
 ~/sbcl/bin/sbcl --no-userinit --disable-debugger --load ~/sbcl/quicklisp/setup.lisp --load build.lisp
 ls ~/sbcl/quicklisp/local-projects/daydreamer/
