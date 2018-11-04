a56cd9a3a50e43c68a6d34454bdd651582ebb7c8
Ed Langley <elangley@cj.com>
2018-11-03 22:53:28 -0700

    Introspection..

--- make.sh
+++ make.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+ls -R ~/sbcl
 ~/sbcl/bin/sbcl --no-userinit --disable-debugger --load ~/sbcl/quicklisp/setup.lisp --load build.lisp
 ls ~/sbcl/quicklisp/local-projects/daydreamer/
 ./daydreamer --self-test
