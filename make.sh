7688e39514e4cb7dfb23969dbb04a52db426bf4a
Ed Langley <elangley@cj.com>
2018-11-04 00:03:00 -0700

    Try workflows?

--- make.sh
+++ make.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 export ASDF_OUTPUT_TRANSLATIONS="/:" SBCL_HOME="$HOME/sbcl/lib/sbcl/" CC="clang"
 
-ls -R ~/sbcl
+env
+
 ~/sbcl/bin/sbcl --no-userinit --disable-debugger --load ~/sbcl/quicklisp/setup.lisp --load build.lisp
 ls ~/sbcl/quicklisp/local-projects/daydreamer/
 ./daydreamer --self-test
