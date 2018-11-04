a1603af081c1efed133cf303f14b9b44dbb0f150
Ed Langley <elangley@cj.com>
2018-11-03 22:31:22 -0700

    update..

---
+++ clone-all-the-things.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+git clone --depth=1 https://gitlab.com/fiddlerwoaroof/osicat.git ~/sbcl/quicklisp/local-projects/osicat.git
+git clone --depth=1 https://gitlab.com/fiddlerwoaroof/cffi.git ~/sbcl/quicklisp/local-projects/cffi.git
+
+git clone --depth=1 https://github.com/fiddlerwoaroof/daydreamer.git ~/sbcl/quicklisp/local-projects/daydreamer/
+git clone --depth=1 https://github.com/fiddlerwoaroof/fwoar.lisputils.git ~/sbcl/quicklisp/local-projects/fwoar.lisputils/
+git clone --depth=1 https://github.com/fiddlerwoaroof/data-lens.git ~/sbcl/quicklisp/local-projects/data-lens/
+git clone --depth=1 https://github.com/pokepay/aws-sdk-lisp.git ~/sbcl/quicklisp/local-projects/aws-sdk-lisp && pushd ~/sbcl/quicklisp/local-projects/aws-sdk-lisp && git checkout 6d1f66e && popd
