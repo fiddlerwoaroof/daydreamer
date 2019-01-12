9f7b05a4bcda18fa51fdf7f8d13fd3ef6aff5ca2
Ed Langley <elangley@cj.com>
2019-01-12 14:35:22 -0800

    sdasldlhasdfweflasef

--- clone-all-the-things.sh
+++ clone-all-the-things.sh
@@ -7,4 +7,4 @@ git clone --depth=1 https://gitlab.com/fiddlerwoaroof/cffi.git ~/quicklisp/local
 
 git clone --depth=1 https://github.com/fiddlerwoaroof/fwoar.lisputils.git ~/quicklisp/local-projects/fwoar.lisputils/
 git clone --depth=1 https://github.com/fiddlerwoaroof/data-lens.git ~/quicklisp/local-projects/data-lens/
-git clone --depth=1 https://github.com/pokepay/aws-sdk-lisp.git ~/quicklisp/local-projects/aws-sdk-lisp && pushd ~/sbcl/quicklisp/local-projects/aws-sdk-lisp && git checkout 6d1f66e && popd
+git clone --depth=1 https://github.com/pokepay/aws-sdk-lisp.git ~/quicklisp/local-projects/aws-sdk-lisp && pushd ~/quicklisp/local-projects/aws-sdk-lisp && git checkout 6d1f66e && popd
