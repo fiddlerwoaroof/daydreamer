b2730fa81ab8ca74286f7a4dcaec37df2effc23d
Ed Langley <elangley@cj.com>
2019-01-12 11:21:32 -0800

    Switch docker images

--- make.sh
+++ make.sh
@@ -3,6 +3,6 @@ export ASDF_OUTPUT_TRANSLATIONS="/:" SBCL_HOME="$HOME/sbcl/lib/sbcl/" CC="clang"
 
 env
 
-~/sbcl/bin/sbcl --no-userinit --disable-debugger --load ~/sbcl/quicklisp/setup.lisp --load build.lisp
+sbcl --no-userinit --disable-debugger --load ~/sbcl/quicklisp/setup.lisp --load build.lisp
 ls ~/sbcl/quicklisp/local-projects/daydreamer/
 ./daydreamer --self-test
