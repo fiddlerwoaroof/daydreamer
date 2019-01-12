0d16c1ec7ce36b90cbed3cc1acbe610ccca8296f
Ed Langley <elangley@cj.com>
2019-01-12 14:33:50 -0800

    Checkout things to the right directory

--- clone-all-the-things.sh
+++ clone-all-the-things.sh
@@ -2,9 +2,9 @@
 cd $HOME
 
 pwd
-git clone --depth=1 https://gitlab.com/fiddlerwoaroof/osicat.git ~/sbcl/quicklisp/local-projects/osicat
-git clone --depth=1 https://gitlab.com/fiddlerwoaroof/cffi.git ~/sbcl/quicklisp/local-projects/cffi
+git clone --depth=1 https://gitlab.com/fiddlerwoaroof/osicat.git ~/quicklisp/local-projects/osicat
+git clone --depth=1 https://gitlab.com/fiddlerwoaroof/cffi.git ~/quicklisp/local-projects/cffi
 
-git clone --depth=1 https://github.com/fiddlerwoaroof/fwoar.lisputils.git ~/sbcl/quicklisp/local-projects/fwoar.lisputils/
-git clone --depth=1 https://github.com/fiddlerwoaroof/data-lens.git ~/sbcl/quicklisp/local-projects/data-lens/
-git clone --depth=1 https://github.com/pokepay/aws-sdk-lisp.git ~/sbcl/quicklisp/local-projects/aws-sdk-lisp && pushd ~/sbcl/quicklisp/local-projects/aws-sdk-lisp && git checkout 6d1f66e && popd
+git clone --depth=1 https://github.com/fiddlerwoaroof/fwoar.lisputils.git ~/quicklisp/local-projects/fwoar.lisputils/
+git clone --depth=1 https://github.com/fiddlerwoaroof/data-lens.git ~/quicklisp/local-projects/data-lens/
+git clone --depth=1 https://github.com/pokepay/aws-sdk-lisp.git ~/quicklisp/local-projects/aws-sdk-lisp && pushd ~/sbcl/quicklisp/local-projects/aws-sdk-lisp && git checkout 6d1f66e && popd
